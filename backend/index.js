d1a9d21503b4eae5edfd7716c762e843f724bdc3
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-08-31 18:05:50 +0530

    Something updated

--- backend/index.js
+++ backend/index.js
@@ -363,7 +363,6 @@ function getDummyQuote(symbol) {
 }
 
 app.use('/api', orderRoutes);
-app.use(express.static(path.join(__dirname, "../frontend/dist/index.html")));
 
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
--- frontend/src/Components/OpenAccount.jsx
+++ frontend/src/Components/OpenAccount.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+import SignUp from './Landing_page/signup/SignUp';
 
 const OpenAccount = () => {
     const navigate = useNavigate();
